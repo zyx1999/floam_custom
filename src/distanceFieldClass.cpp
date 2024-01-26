ca90e093bb0bf000fd6f4c473b3c7c7dd330af61
YuxuanZhao <18218490078@163.com>
2024-01-26 16:10:59 +0800

    remove old sdf compute method

--- src/distanceFieldClass.cpp
+++ src/distanceFieldClass.cpp
@@ -1,42 +1,6 @@
 #include "distanceFieldClass.h"
 #include "signed_distance_field_2d.h"
 
-// void DistanceField::computeDistanceField(
-//     const pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud,
-//     pcl::PointCloud<pcl::PointXYZI>::Ptr& distance_field)
-// {
-//     Eigen::Vector3f grid_min(-sdf_x_bound_, -sdf_y_bound_, sdf_z_lower_);
-//     Eigen::Vector3f grid_max(sdf_x_bound_, sdf_y_bound_, sdf_z_upper_);
-//     // 创建 KdTree 以加速最近邻搜索
-//     pcl::KdTreeFLANN<pcl::PointXYZI> kdtree;
-//     kdtree.setInputCloud(cloud);
-//     // 遍历体素网格
-//     for (float x = grid_min.x(); x <= grid_max.x(); x += sdf_resolution_) {
-//         for (float y = grid_min.y(); y <= grid_max.y(); y += sdf_resolution_) {
-//             // for (float z = grid_min.z(); z <= grid_max.z(); z += sdf_resolution_) {
-//                 float z = sdf_z_lower_;
-//                 pcl::PointXYZI searchPoint;
-//                 searchPoint.x = x;
-//                 searchPoint.y = y;
-//                 searchPoint.z = z;
-//                 // 执行最近邻搜索
-//                 std::vector<int> pointIdxNKNSearch(1);
-//                 std::vector<float> pointNKNSquaredDistance(1);
-//                 if (kdtree.nearestKSearch(searchPoint, 1, pointIdxNKNSearch,
-//                                           pointNKNSquaredDistance) > 0) {
-//                     // 计算距离并存储在距离场中
-//                     pcl::PointXYZI point;
-//                     point.x         = x;
-//                     point.y         = y;
-//                     point.z         = z;
-//                     point.intensity = std::sqrt(pointNKNSquaredDistance[0]);
-//                     distance_field->push_back(point);
-//                 }
-//             // }
-//         }
-//     }
-// }
-
 void DistanceField::ComputeSignedDistanceField(
     const pcl::PointCloud<pcl::PointXYZI>::Ptr& cloud_in,
     pcl::PointCloud<pcl::PointXYZI>::Ptr& signed_distance_field)
